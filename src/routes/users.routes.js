ecfc2bea176fe0a6c4eeb651f9df85514ed701b7
Marcelo Cerqueira <marcelocerqueira661@gmail.com>
2022-11-21 21:33:41 -0300

    inicializando o commint

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,8 +4,14 @@ const UsersController = require("../controllers/usersControllers")
 
 const usersRoutes = Router();
 
+function myMiddleware(request, response, next) {
+  console.log("Você passou pelo Middleware.")
+  next();
+}
+
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
+usersRoutes.put("/:id", usersController.update);
 
 module.exports = usersRoutes;
